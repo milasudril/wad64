92d12ad9566aab224d8ec57fb9c311e8fb54959d
Torbjörn Rathsman <milasudril@gmail.com>
2021-01-10 10:00:58 +0100

    Add comments to structs

--- lib/file_structs.hpp
+++ lib/file_structs.hpp
@@ -9,8 +9,16 @@ namespace Wad64
 	*/
 	struct WadInfo
 	{
+		/** File magic number. Should read xWAD64'\n''\0'
+		 */
 		std::byte identification[8];
+
+		/** The number of lumps in this file
+		*/
 		int64_t numlumps;
+
+		/** The offset, from the beginning of file, to the directory
+		*/
 		int64_t infotablesofs;
 	};
 
@@ -18,8 +26,15 @@ namespace Wad64
 	 */
 	struct FileLump
 	{
+		/** The offset, from the beginning of file, to this lump
+		*/
 		int64_t filepos;
+		/** The size of this lump
+		*/
 		int64_t size;
+
+		/** The name of this lump (encoded in UTF-8). The name must not contain a ascii nul char.
+		*/
 		char8_t name[256 - 16];
 	};
 }
