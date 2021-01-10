40897794706e6351b161b959e16b5905bfe08df7
Torbjörn Rathsman <milasudril@gmail.com>
2021-01-10 11:43:57 +0100

    More work on IoPolicy

--- lib/io_policy.hpp
+++ lib/io_policy.hpp
@@ -6,6 +6,7 @@
 #include <utility>
 #include <cstdint>
 #include <span>
+#include <functional>
 
 namespace Wad64
 {
@@ -15,6 +16,56 @@ namespace Wad64
 		{read(a, std::declval<std::span<std::byte>>())} -> std::same_as<std::size_t>;
 		{write(a, std::declval<std::span<std::byte const>>())} -> std::same_as<std::size_t>;
 		{seek(a, std::declval<int64_t>())} -> std::same_as<std::int64_t>;
+	};
+
+	namespace detail
+	{
+		template<RandomAccessFile File>
+		auto read(void* handle, std::span<std::byte> buffer)
+		{
+			return read(*reinterpret_cast<File*>(handle), buffer);
+		}
+
+		template<RandomAccessFile File>
+		auto write(void* handle, std::span<std::byte const> buffer)
+		{
+			return write(*reinterpret_cast<File*>(handle), buffer);
+		}
+
+		template<RandomAccessFile File>
+		auto seek(void* handle, int64_t offset)
+		{
+			return seek(*reinterpret_cast<File*>(handle), offset);
+		}
+	}
+
+	class FileReference
+	{
+	public:
+		template<RandomAccessFile File>
+		explicit FileReference(std::reference_wrapper<File> file):
+			 m_ref{&file.get()}
+			,m_read{detail::read<File>}
+			,m_write{detail::write<File>}
+			,m_seek{detail::seek<File>}
+			{}
+
+		size_t read(std::span<std::byte> buffer) const
+		{ return m_read(m_ref, buffer); }
+
+		size_t write(std::span<std::byte const> buffer) const
+		{ return m_write(m_ref, buffer); }
+
+		int64_t seek(int64_t offset) const
+		{ return m_seek(m_ref, offset); }
+
+		void* handle() const { return m_ref; }
+
+	private:
+		void* m_ref;
+		size_t (*m_read)(void*, std::span<std::byte>);
+		size_t (*m_write)(void*, std::span<std::byte const>);
+		int64_t (*m_seek)(void*, int64_t);
 	};
 }
 
--- lib/io_policy.test.cpp
+++ lib/io_policy.test.cpp
@@ -2,7 +2,55 @@
 
 #include "./io_policy.hpp"
 
+#include <string>
+#include <cassert>
+
+namespace
+{
+	struct File
+	{
+		int64_t last_seek_offset;
+	};
+
+	size_t read(File&, std::span<std::byte> buffer)
+	{
+		return std::size(buffer);
+	}
+
+	size_t write(File&, std::span<std::byte const> buffer)
+	{
+		return std::size(buffer);
+	}
+
+	int64_t seek(File& f, int64_t val)
+	{
+		f.last_seek_offset = val;
+		return val;
+	}
+}
+
+namespace Testcases
+{
+	void wad64FileReferenceCopy()
+	{
+		File f;
+		Wad64::FileReference foo{std::ref(f)};
+		auto bar = foo;
+		assert(bar.handle() == foo.handle());
+	}
+
+	void wad64FileReferenceSeek()
+	{
+		File f{};
+		Wad64::FileReference foo{std::ref(f)};
+		foo.seek(12);
+		assert(f.last_seek_offset == 12);
+	}
+}
+
 int main()
 {
+	Testcases::wad64FileReferenceCopy();
+	Testcases::wad64FileReferenceSeek();
 	return 0;
 }
