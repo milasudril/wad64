3618c267b490221062b93e0a73e645272beeaf28
Torbjörn Rathsman <milasudril@gmail.com>
2021-01-10 12:03:00 +0100

    Add API specification for Archive

--- lib/archive.hpp
+++ lib/archive.hpp
@@ -5,8 +5,14 @@
 
 #include "./io_policy.hpp"
 
+#include <map>
+#include <string>
+
 namespace Wad64
 {
+	class InputFile;
+	class OutputFile;
+
 	class Archive
 	{
 	public:
@@ -15,8 +21,25 @@ namespace Wad64
 		{
 		}
 
+		InputFile open(std::u8string_view filename) const && = delete;
+		OutputFile open(std::u8string_view filename) && = delete;
+		OutputFile create(std::u8string_view filename) && = delete;
+
+		InputFile open(std::u8string_view filename) const &;
+
+		OutputFile open(std::u8string_view filename) &;
+
+		OutputFile create(std::u8string_view filename) &;
+
 	private:
 		FileReference m_file_ref;
+		struct DirEntry
+		{
+			int64_t offset;
+			int64_t size;
+		};
+
+		std::map<std::u8string, DirEntry> m_directory;
 	};
 }
 
