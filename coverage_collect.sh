c6bde0a2b4ef288149fe841c28e11bb17cb5eddb
Torbjörn Rathsman <milasudril@gmail.com>
2021-01-11 22:20:29 +0100

    Code coverage integration

---
+++ coverage_collect.sh
@@ -0,0 +1,4 @@
+mkdir __targets/.coverage 2> /dev/null || true
+cd __targets/.coverage
+gcovr -p --html-details coverage.html --root ../../
+xdg-open coverage.html
---
+++ testrunner.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+"$1"
+rm *.gcda
+rm *.gcno
