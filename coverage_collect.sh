d150c98152f580fa97858339feb3cf0d58c54431
Torbjörn Rathsman <milasudril@gmail.com>
2021-01-12 19:31:28 +0100

    Initial testcase for Archive

--- coverage_collect.sh
+++ coverage_collect.sh
@@ -1,4 +1,3 @@
 mkdir __targets/.coverage 2> /dev/null || true
 cd __targets/.coverage
 gcovr -p --html-details coverage.html --root ../../
-xdg-open coverage.html
--- lib/archive.test.cpp
+++ lib/archive.test.cpp
@@ -2,4 +2,23 @@
 
 #include "archive.hpp"
 
-int main() {}
+#include "./membuffer.hpp"
+#include <cassert>
+
+
+namespace Testcases
+{
+	void wad64LoadEmptyArchive()
+	{
+		Wad64::MemBuffer buff;
+		Wad64::Archive archive{std::ref(buff)};
+
+		assert(archive.fileReference().handle() == &buff);
+		assert(std::size(archive.ls()) == 0);
+	}
+}
+
+int main()
+{
+	Testcases::wad64LoadEmptyArchive();
+}
