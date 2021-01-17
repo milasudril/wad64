964ef1d2044bb7e974678f34bd6db51bda4017d0
Torbjörn Rathsman <milasudril@gmail.com>
2021-01-17 10:04:37 +0100

    Make FdAdapter functions more robust

--- lib/fd_adapter.cpp
+++ lib/fd_adapter.cpp
@@ -4,12 +4,49 @@
 
 #include "./fd_adapter.hpp"
 
+#include <stdexcept>
+
 size_t Wad64::read(FdAdapter fd, std::span<std::byte> buffer, int64_t offset)
 {
-	return ::pread(fd.fd, std::data(buffer), std::size(buffer), offset);
+	auto ptr = std::data(buffer);
+	auto const end = std::data(buffer) + std::size(buffer);
+	while(ptr != end)
+	{
+		auto const bytes_left = end - ptr;
+		auto const n_read = ::pread(fd.fd, ptr, bytes_left, offset);
+		if(n_read == -1) [[unlikely]]
+		{
+			if(errno == EAGAIN || errno == EWOULDBLOCK) [[likely]] { continue; }
+			throw std::runtime_error{"I/O error"};
+		}
+		if(n_read == 0)
+		{ return ptr - std::data(buffer); }
+		offset += n_read;
+		ptr += n_read;
+	}
+	return std::size(buffer);
 }
 
 size_t Wad64::write(FdAdapter fd, std::span<std::byte const> buffer, int64_t offset)
 {
-	return ::pwrite(fd.fd, std::data(buffer), std::size(buffer), offset);
+	auto ptr = std::data(buffer);
+	auto const end = std::data(buffer) + std::size(buffer);
+	while(ptr != end)
+	{
+		auto const bytes_left = end - ptr;
+		auto const n_written = ::pwrite(fd.fd, std::data(buffer), bytes_left, offset);
+		if(n_written == -1) [[unlikely]]
+		{
+			if(errno == EAGAIN || errno == EWOULDBLOCK) [[likely]] { continue; }
+			throw std::runtime_error{"I/O error"};
+		}
+
+		if(n_written == 0)
+		{ return ptr - std::data(buffer); }
+
+		offset += n_written;
+		ptr += n_written;
+
+	}
+	return std::size(buffer);
 }
