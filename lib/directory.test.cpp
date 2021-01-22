8ea065e487e3c059fdd4ca6447fd8433fb2fef39
Torbjörn Rathsman <milasudril@gmail.com>
2021-01-22 20:27:05 +0100

    Add testdriver for Directory

---
+++ lib/directory.test.cpp
@@ -0,0 +1,16 @@
+//@	{
+//@	 "targets":[{"name":"directory.test","type":"application", "autorun":1}]
+//@	}
+
+#include "./directory.hpp"
+
+#include <cassert>
+
+namespace Testcases
+{
+}
+
+int main()
+{
+	return 0;
+}
