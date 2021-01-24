3bcbfeda5ee5d7b3182f9f9935d2561f13bccc8e
Torbjörn Rathsman <milasudril@gmail.com>
2021-01-24 18:10:38 +0100

    Add testdriver for archive view

---
+++ lib/archive_view.test.cpp
@@ -0,0 +1,14 @@
+//@	{
+//@	 "targets":[{"name":"archive_view.test","type":"application", "autorun":1}]
+//@	}
+
+#include "./archive_view.hpp"
+
+namespace Testcases
+{
+}
+
+int main()
+{
+	return 0;
+}
