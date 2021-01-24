5c08cb18b747d3ad476eabddf28172ef141c6e80
Torbjörn Rathsman <milasudril@gmail.com>
2021-01-24 18:56:16 +0100

    Add wrapper file

---
+++ lib/wad64.hpp
@@ -0,0 +1,13 @@
+//@	{"targets":[{"name":"wad64.hpp","type":"include"}]}
+
+#ifndef WAD64_LIB_WAD64_HPP
+#define WAD64_LIB_WAD64_HPP
+
+#include "./archive.hpp"
+#include "./archive_view.hpp"
+
+namespacce Wad64
+{
+}
+
+#endif
