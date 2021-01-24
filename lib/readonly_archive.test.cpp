f0c2002f30727b8ac115ff2e3d21b600e53de9d7
Torbjörn Rathsman <milasudril@gmail.com>
2021-01-24 15:25:42 +0100

    Add testcase for readonly archive

---
+++ lib/readonly_archive.test.cpp
@@ -0,0 +1,22 @@
+//@	{
+//@	 "targets":[{"name":"readonly_archive.test","type":"application", "autorun":1}]
+//@	}
+
+#include "./readonly_archive.hpp"
+
+#include "./fd_owner.hpp"
+
+namespace Testcases
+{
+	void wad64ReadonlyArchiveCreateFromReadonlyFile()
+	{
+		Wad64::FdOwner src{"testdata/file_with_data.wad64", Wad64::IoMode::AllowRead(), Wad64::FileCreationMode::AllowOverwrite()};
+		Wad64::ReadonlyArchive archive{Wad64::FileReference{std::ref(src)}};
+	}
+}
+
+int main()
+{
+	Testcases::wad64ReadonlyArchiveCreateFromReadonlyFile();
+	return 0;
+}
